e568b77ae854e76e2c3276a97d9db70e3833f777
Robin Bertuccelli <robin.bertuccelli@gmail.com>
2018-05-03 20:08:03 -0500

    added vertMenu and Footer

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,20 @@
 import React, { Component } from 'react';
 import Nav from './components/Nav';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
-
+import Footer from './components/Footer';
 import Candidates from './pages/candidates';
 import Home from './pages/home';
 
 
 const App = () => (
-  <div> 
-    <Nav />
+    <div>
+      <Nav />
+      <Router>
 
-    
-
-   
-
-  </div>
-
+        <Route exact path="/" component={Home} />
+  		</Router>
+  	<Footer />
+  	</div>
 );
 
 
