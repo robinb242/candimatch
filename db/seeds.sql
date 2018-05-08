844ae7388c7b04509de67d31898bcafca0bd1733
LeAnnS <leann.sawatzky@gmail.com>
2018-05-08 16:15:46 -0500

    some formatting on questionnaire

---
+++ db/seeds.sql
@@ -0,0 +1,18 @@
+-- Insert a set of records.
+
+USE issues_db;
+
+INSERT INTO issues (issue_name, question_1, question_2, question_3, answer_q1, answer_q2, answer_q3) 
+VALUES ('Issue-A', 'Question-A1', 'Question-A2', 'Question-A3'),
+
+
+
+
+USE issues_db;
+ createdAt
+INSERT INTO user (user_name, user_pwd, issue_name, answer_q1, answer_q2, answer_q3),
+VALUES ('User-1', 'Pwd-1','Ans-A1', 'Ans-A2', 'Ans-A3'),
+
+-- MySQL will not take a path to upload a file, it must be handled by fs
+
+-- Good example here:  http://www.technicalkeeda.com/nodejs-tutorials/nodejs-store-image-into-mysql-database
