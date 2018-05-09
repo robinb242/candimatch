3a2cca679ebeb1edf8a05fc3c4a0b32d71a03852
Max Adams <max.adams34@gmail.com>
2018-05-09 13:36:05 -0500

    database updates

--- db/schema.sql
+++ db/schema.sql
@@ -1,29 +1,69 @@
 -- Create the database doggo_db and specified it for use.
-CREATE DATABASE issues_db;
-USE issues_db;
+CREATE DATABASE answers_db;
+USE answers_db;
 
 -- Create the table dog and park
-CREATE TABLE questions
+CREATE TABLE can_ans1
 (
 id int NOT NULL AUTO_INCREMENT,
 issue_name varchar(150) NOT NULL,
-question_1 varchar(150) NOT NULL,
-question_2 varchar(150) NOT NULL,
-question_3 varchar(150) NOT NULL,
+question_1 integer DEFAULT 0,
+question_2 integer DEFAULT 0,
+question_3 integer DEFAULT 0,
 
 PRIMARY KEY (id)
 );
 
-CREATE TABLE user
+CREATE TABLE can_ans2
 (
 id int NOT NULL AUTO_INCREMENT,
-user_name varchar(150) NOT NULL,
-user_password varchar(150) NOT NULL,
 issue_name varchar(150) NOT NULL,
-answer_q1 varchar(2) DEFAULT "0"
-answer_q2 varchar(2) DEFAULT "0"
-answer_q3 varchar(2) DEFAULT "0"
+question_1 integer DEFAULT 0,
+question_2 integer DEFAULT 0,
+question_3 integer DEFAULT 0,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE can_ans3
+(
+id int NOT NULL AUTO_INCREMENT,
+issue_name varchar(150) NOT NULL,
+question_1 integer DEFAULT 0,
+question_2 integer DEFAULT 0,
+question_3 integer DEFAULT 0,
 
 PRIMARY KEY (id)
 );
 
+CREATE TABLE user_ans1
+(
+id int NOT NULL AUTO_INCREMENT,
+issue_name varchar(150) NOT NULL,
+answer_1 integer DEFAULT 0,
+answer_2 integer DEFAULT 0,
+answer_3 integer DEFAULT 0,
+
+PRIMARY KEY (id)
+);
+
+CREATE TABLE user_ans2
+(
+id int NOT NULL AUTO_INCREMENT,
+issue_name varchar(150) NOT NULL,
+answer_1 integer DEFAULT 0,
+answer_2 integer DEFAULT 0,
+answer_3 integer DEFAULT 0,
+
+PRIMARY KEY (id)
+);
+
+CREATE TABLE user_ans3
+(
+id int NOT NULL AUTO_INCREMENT,
+issue_name varchar(150) NOT NULL,
+answer_1 integer DEFAULT 0,
+answer_2 integer DEFAULT 0,
+answer_3 integer DEFAULT 0,
+
+PRIMARY KEY (id)
+);
