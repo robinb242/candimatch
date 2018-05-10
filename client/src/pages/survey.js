6980bb318e4c72c8912ed9f8165da1be5bfbdab6
LeAnnS <leann.sawatzky@gmail.com>
2018-05-09 23:03:58 -0500

    added userpage, candidate log in pages, and links

--- client/src/pages/survey.js
+++ client/src/pages/survey.js
@@ -3,44 +3,56 @@ import VertMenu from '../components/VertMenu';
 import { Menu, Grid, Container, Divider, Image, Button } from 'semantic-ui-react'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Questions from '../components/Questions';
+import {Link} from 'react-router-dom';
 
 
-const Survey = () => (
-		// console.log ("handlePageLoad function");
+class Survey extends React.Component {
+	handleClick = (event) => {
+		console.log ("userPage loaded");
+	};
 
-  <div>
-    <Grid>
-    	<Grid.Row>
-    		<Grid.Column width={16}>
+	render() {
+	  return <div>
+		<Container>
+	      <Grid>
+	    	<Grid.Row>
+	    		<Grid.Column width={16}>
 
-		      <Container textAlign='center'>
-		      	<b>TAKE THE SURVEY TO FIND A CANDIDATE WHOSE VALUES MOST CLOSELY MATCH YOUR VALUES.</b>
-		          
-		        <Divider />
+			      <Container textAlign='center'>
+			      	<b>TAKE THE SURVEY TO FIND A CANDIDATE WHOSE VALUES MOST CLOSELY MATCH YOUR VALUES.</b>
+			          
+			        <Divider />
 
-		        <p>Review the following statements regarding each of the issues and select the column that most closely matches your level of agreement.</p>
-		        
-		        <p>More Instructions.</p>
+			        <p>Review the following statements regarding each of the issues and select the column that most closely matches your level of agreement.</p>
+			        
+			        <p>More Instructions.</p>
 
-		        <div>
-		        	<Questions />
-		       	</div>
-		         		        		       
-		       	<h4>You have finished the survey. Click the button below to find your matching candidate.</h4>
-		       
-		       	<br />
+			        <div>
+			        	<Questions />
+			       	</div>
+			         		        		       
+			       	<h4>You have finished the survey. Click the button below to find your matching candidate.</h4>
+			       
+			       	<br />
 
-				<div className ="ui inverted segment">
-		          <button className="ui inverted green huge button fluid">Submit Survey</button>
-		        </div>
+					<div className ="ui inverted segment">
+						<Link
+						to="/userPage"
+							onClick={this.handleClick}
+				          	className="ui inverted huge button fluid">Submit Survey
+			          	</Link>
+			        </div>
 
+			      </Container>
 
-		      </Container> 
-    		</Grid.Column>
-    	</Grid.Row>
-    </Grid>
-  </div>
+	    		</Grid.Column>
+	    	</Grid.Row>
+    	  </Grid>
+    	</Container>
+  	  </div>
 
-);
+  	}
+
+}
   
 export default Survey;
--- client/src/pages/userPage.js
+++ client/src/pages/userPage.js
@@ -1,99 +1,117 @@
 import React, { Component } from 'react'
-import { Icon, Label, Menu, Table } from 'semantic-ui-react'
+import { Icon, Label, Menu, Container, Table } from 'semantic-ui-react'
 
 const UserPage = () => (
 
   <div>
+    <Container>
 
-    <Table celled>
-      <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell>Question</Table.HeaderCell>
-          <Table.HeaderCell>My Answers</Table.HeaderCell>
-          <Table.HeaderCell>Candidate Answers</Table.HeaderCell>
-        </Table.Row>
-      </Table.Header>
+      <Table celled>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell width={10}>Question</Table.HeaderCell>
+            <Table.HeaderCell width={2} textAlign='center'>My Answers</Table.HeaderCell>
+            <Table.HeaderCell width={2} textAlign='center'>Candidate-1 Answers</Table.HeaderCell>
+            <Table.HeaderCell width={2} textAlign='center'>Candidate-2 Answers</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
 
-      <Table.Body>
+        <Table.Body>
 
-        <Table.Row>
-          <Table.Cell>
-            <Label ribbon>Issue 'A'</Label>
-          </Table.Cell>
-          <Table.Cell>Question A-1</Table.Cell>
-          <Table.Cell>My_Ans-A1</Table.Cell>
-          <Table.Cell>Cand_Ans-A1</Table.Cell>
-        </Table.Row>
-        <Table.Row>
-          <Table.Cell>Question A-2</Table.Cell>
-          <Table.Cell>My_Ans-A2</Table.Cell>
-          <Table.Cell>Cand_Ans-A2</Table.Cell>
-        </Table.Row>
-        <Table.Row>
-          <Table.Cell>Question A-3</Table.Cell>
-          <Table.Cell>My_Ans-A3</Table.Cell>
-          <Table.Cell>Cand_Ans-A3</Table.Cell>
-        </Table.Row>
+          <Table.Row>
+            <Table.Cell colSpan='3'>
+              <Label ribbon>Issue 'A'</Label>
+            </Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Question A-1</Table.Cell>
+            <Table.Cell>My_Ans-A1</Table.Cell>
+            <Table.Cell>Cand1_Ans-A1</Table.Cell>
+            <Table.Cell>Cand2_Ans-A1</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Question A-2</Table.Cell>
+            <Table.Cell>My_Ans-A2</Table.Cell>
+            <Table.Cell>Cand1_Ans-A2</Table.Cell>
+            <Table.Cell>Cand2_Ans-A2</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Question A-3</Table.Cell>
+            <Table.Cell>My_Ans-A3</Table.Cell>
+            <Table.Cell>Cand1_Ans-A3</Table.Cell>
+            <Table.Cell>Cand2_Ans-A3</Table.Cell>
+          </Table.Row>
 
-         <Table.Row>
-          <Table.Cell>
-            <Label ribbon>Issue 'B'</Label>
-          </Table.Cell>
-          <Table.Cell>Question B-1</Table.Cell>
-          <Table.Cell>My_Ans-B1</Table.Cell>
-          <Table.Cell>Cand_Ans-B1</Table.Cell>
-        </Table.Row>
-        <Table.Row>
-          <Table.Cell>Question B-2</Table.Cell>
-          <Table.Cell>My_Ans-B2</Table.Cell>
-          <Table.Cell>Cand_Ans-B2</Table.Cell>
-        </Table.Row>
-        <Table.Row>
-          <Table.Cell>Question B-3</Table.Cell>
-          <Table.Cell>My_Ans-B3</Table.Cell>
-          <Table.Cell>Cand_Ans-B3</Table.Cell>
-        </Table.Row>
+          <Table.Row>
+            <Table.Cell colSpan='3'>
+              <Label ribbon>Issue 'B'</Label>
+            </Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Question B-1</Table.Cell>
+            <Table.Cell>My_Ans-B1</Table.Cell>
+            <Table.Cell>Cand1_Ans-B1</Table.Cell>
+            <Table.Cell>Cand2_Ans-B1</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Question B-2</Table.Cell>
+            <Table.Cell>My_Ans-B2</Table.Cell>
+            <Table.Cell>Cand1_Ans-B2</Table.Cell>
+            <Table.Cell>Cand2_Ans-B2</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Question B-3</Table.Cell>
+            <Table.Cell>My_Ans-B3</Table.Cell>
+            <Table.Cell>Cand1_Ans-B3</Table.Cell>
+            <Table.Cell>Cand2_Ans-B3</Table.Cell>
+          </Table.Row>
 
-        <Table.Row>
-          <Table.Cell>
-            <Label ribbon>Issue 'C'</Label>
-          </Table.Cell>
-          <Table.Cell>Question C-1</Table.Cell>
-          <Table.Cell>My_Ans-C1</Table.Cell>
-          <Table.Cell>Cand_Ans-C1</Table.Cell>
-        </Table.Row>
-        <Table.Row>
-          <Table.Cell>Question C-2</Table.Cell>
-          <Table.Cell>My_Ans-C2</Table.Cell>
-          <Table.Cell>Cand_Ans-C2</Table.Cell>
-        </Table.Row>
-        <Table.Row>
-          <Table.Cell>Question C-3</Table.Cell>
-          <Table.Cell>My_Ans-C3</Table.Cell>
-          <Table.Cell>Cand_Ans-C3</Table.Cell>
-        </Table.Row>
+          <Table.Row>
+            <Table.Cell colSpan='3'>
+              <Label ribbon>Issue 'C'</Label>
+            </Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Question C-1</Table.Cell>
+            <Table.Cell>My_Ans-C1</Table.Cell>
+            <Table.Cell>Cand1_Ans-C1</Table.Cell>
+            <Table.Cell>Cand2_Ans-C1</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Question C-2</Table.Cell>
+            <Table.Cell>My_Ans-C2</Table.Cell>
+            <Table.Cell>Cand1_Ans-C2</Table.Cell>
+            <Table.Cell>Cand2_Ans-C2</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Question C-3</Table.Cell>
+            <Table.Cell>My_Ans-C3</Table.Cell>
+            <Table.Cell>Cand1_Ans-C3</Table.Cell>
+            <Table.Cell>Cand2_Ans-C3</Table.Cell>
+          </Table.Row>
 
-      </Table.Body>
+        </Table.Body>
 
-      <Table.Footer>
-        <Table.Row>
-          <Table.HeaderCell colSpan='3'>
-            <Menu floated='right' pagination>
-              <Menu.Item as='a' icon>
-                <Icon name='chevron left' />
-              </Menu.Item>
-              <Menu.Item as='a'>1</Menu.Item>
-              <Menu.Item as='a'>2</Menu.Item>
-              <Menu.Item as='a'>3</Menu.Item>
-              <Menu.Item as='a'>4</Menu.Item>
-              <Menu.Item as='a' icon>
-                <Icon name='chevron right' />
-              </Menu.Item>
-            </Menu>
-          </Table.HeaderCell>
-        </Table.Row>
-      </Table.Footer>
-    </Table>
+        <Table.Footer>
+          <Table.Row>
+            <Table.HeaderCell colSpan='3'>
+              <Menu floated='right' pagination>
+                <Menu.Item as='a' icon>
+                  <Icon name='chevron left' />
+                </Menu.Item>
+                <Menu.Item as='a'>1</Menu.Item>
+                <Menu.Item as='a'>2</Menu.Item>
+                <Menu.Item as='a'>3</Menu.Item>
+                <Menu.Item as='a'>4</Menu.Item>
+                <Menu.Item as='a' icon>
+                  <Icon name='chevron right' />
+                </Menu.Item>
+              </Menu>
+            </Table.HeaderCell>
+          </Table.Row>
+        </Table.Footer>
+      </Table>
+    </Container>
 
   </div>
 );
