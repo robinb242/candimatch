133a9b057278af82ac615f435fddbae4a9e1c1bd
Robin Bertuccelli <robin.bertuccelli@gmail.com>
2018-05-12 06:22:39 -0500

    read me

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -8,9 +8,7 @@ const Footer = () => (
         <div class="ui center aligned container">
             <h5 id="footerTitle" className="ui inverted header">&copy; Copyright 2018 | All rights reserved | CandiMatch</h5>
             <h6 className="ui inverted header">&copy; Created By: Max Adams, Robin Bertuccelli, LeAnn Sawatzky and Chris Yourk </h6>
-            <a href="https://www.facebook.com/"><i class="facebook square icon big"></i></a>
-            <a href="https://twitter.com/"><i class="twitter square icon big"></i></a>
-            <a href="https://www.linkedin.com/company/c"><i class="linkedin square icon big"></i></a>
+           
         </div>
     </div>
  </Container>
